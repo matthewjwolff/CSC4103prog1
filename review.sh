b2a480e42de8ffd8e579b9524eea2a9eec98f665
Matthew Wolff <mwolff3@lsu.edu>
2016-09-09 20:01:46 -0500

    added review script

---
+++ review.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#Store current directory
+DIR=$(pwd)
+#If we were given a directory
+if [[ $1 ]]
+then
+	# Go to it
+	cd $1
+fi
+
+# Source the README file, break things formatted like ``this`` into lines
+# Pipe the contents of the file to grep, grep returns all matches of two backticks followed by anything without whitespace that is also followed by two backticks
+FILES=$(cat README.rst | grep -o "\`\`[^ ]*\`\`")
+
+# Simple for loop
+for FILE in $FILES
+do
+	# Take off the backticks (create a substring two in from the left and two in from the right
+	NOTICK=${FILE:2:-2}
+	# Do a test run of highlight to see if it can understand the file extension
+	# (better than using the file command)
+	ERROR=$(
+		# Execute the command, but dump output to /dev/null
+		(highlight -O ansi $NOTICK > /dev/null)
+		# Route the stderr to stdout so it will be store to ERROR
+		2>&1
+	)
+	# If ERROR is empty
+	if [[ -z ERROR ]]
+	then
+		# Pretty print code, pipe to nl for line numbers
+		highlight -O ansi $NOTICK | nl
+		# wait for user ENTER
+		read whocares
+		clear
+	fi
+done
+
+# return user to directory at start of script
+# Use double quotes to handle paths with whitespace in them
+cd "$DIR"
